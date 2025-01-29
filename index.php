b1c4e4d8c9878925ff97734e4a529206f481b826
Pedro <pedrottorres07@gmail.com>
2025-01-29 17:09:09 -0300

    Imagens do Projeto

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hamburgueria do Pedrão</title>
-
+    <link rel="stylesheet" href="Css/output.css">
     <link rel="stylesheet" href="CSS/Styles.css">
+    
+    <title>Hamburgueria do Pedrão</title>
+   
 </head>
 <body>
   <h1>Inicio do Projeto</h1>  
