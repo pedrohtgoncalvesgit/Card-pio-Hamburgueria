ba37b00e7190d6b255d3502d68fa7d91a1c545b9
Pedro <pedrottorres07@gmail.com>
2025-01-29 16:26:58 -0300

    Inicio do Projeto

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hamburgueria do Pedrão</title>
+
+    <link rel="stylesheet" href="CSS/Styles.css">
 </head>
 <body>
-    Oie
+  <h1>Inicio do Projeto</h1>  
 </body>
 </html>
